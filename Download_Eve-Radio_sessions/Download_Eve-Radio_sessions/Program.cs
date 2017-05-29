6b1502de1958a27081adb5c189be319575570491
Joren <jorenvandamme@hotmail.com>
2017-05-29 11:22:24 +0200

    WPF versie aangemaakt - Controle op dubbelen
    - WPF versie aangemaakt
    - Controle om te zien of een bestand al gedownload is.

--- Download_Eve-Radio_sessions/Download_Eve-Radio_sessions/Program.cs
+++ Download_Eve-Radio_sessions/Download_Eve-Radio_sessions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -11,43 +12,90 @@ namespace Download_Eve_Radio_sessions
     {
         static void Main(string[] args)
         {
+            MainAsync(args).Wait();//Alles downloaden
+
+            Console.WriteLine("Alles is gedownload");
+            Console.ReadKey();
+        }
+
+        //Alles downloaden
+        static async Task MainAsync(string[] args)
+        {
             //EVE Radio heeft leuke setjes die je tot 2 weken terug kan herbeluisteren. 
             //Nu wil ik die setjes langer kunnen beluisteren en daarom download ik die.
             //Aangezien ik te lui ben om dit elke keer handmatig te doen schrijven we hier ene programmaatje voor.
-
-            using(WebClient client = new WebClient())
+            try
             {
-                //We downloaden de EVE-Radio pagina
-                string htmlCode = client.DownloadString("http://eve-radio.com/radio/rewind");
-
-                //We splitsen de herbeluister divs op basis van hun ID
-                string[] stringSeperators = new string[] { "<div id='erRW' style='float: left;'>" };
-                string[] rewinds = htmlCode.Split(stringSeperators, StringSplitOptions.None).Skip(1).ToArray();
-                //herbeluisters.Skip(1).ToArray();//Het eerste element eruit halen.
-
-                //Nu gaan we voor elk van de gevonden rewinds de starturl opzoeken
-                foreach(string rewind in rewinds)
+                using(WebClient client = new WebClient())
                 {
-                    //client.DownloadProgressChanged += Client_DownLoadProcessChanged;
+                    //We downloaden de EVE-Radio pagina
+                    string htmlCode = client.DownloadString("http://eve-radio.com/radio/rewind");
 
-                    //We vinden de startpositie van de tekst die we willen, de eindpositie, en halen daar de lengte uit.
-                    int startPos = rewind.IndexOf("Listen from: <a href='#' onclick=\"javascript:doCmd({rewind:'") + "Listen from: <a href='#' onclick=\"javascript:doCmd({rewind:'".Length;
-                    int length = rewind.IndexOf("'}); return false;\">Start") - startPos;
+                    //We splitsen de herbeluister divs op basis van hun ID
+                    string[] stringSeperators = new string[] { "<div id='erRW' style='float: left;'>" };
+                    string[] rewinds = htmlCode.Split(stringSeperators, StringSplitOptions.None).Skip(1).ToArray();//Het eerste dat we eruit halen is rommel.
 
-                    string downloadUrl = rewind.Substring(startPos, length);
+                    //Nu gaan we voor elk van de gevonden rewinds de starturl opzoeken
+                    foreach(string rewind in rewinds)
+                    {
+                        client.DownloadProgressChanged += Client_DownLoadProcessChanged;
 
-                    client.DownloadFile(downloadUrl, "test.mp3");
-                    //client.DownloadFileAsync(new Uri(downloadUrl), "test.mp3");
+                        //We vinden de startpositie van de tekst die we willen, de eindpositie, en halen daar de lengte uit.
+                        int startPos = rewind.IndexOf("Listen from: <a href='#' onclick=\"javascript:doCmd({rewind:'") + "Listen from: <a href='#' onclick=\"javascript:doCmd({rewind:'".Length;
+                        int length = rewind.IndexOf("'}); return false;\">Start") - startPos;
+
+                        string downloadUrl = rewind.Substring(startPos, length);
+
+                        //Proberen met een proxy dingen te omzeilen
+                        WebProxy wp = new WebProxy("35.161.2.200", 80);
+                        client.Proxy = wp;
+
+                        //Nakijken of dat proxy werkt?
+
+                        //Make session-unique name
+                        string filename = downloadUrl.Split('/').Last();
+                        string path = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+                        //Check if we didn't already download this file.
+                        if(!File.Exists(filename))
+                        {
+                            //client.DownloadFile(downloadUrl, "test.mp3");//Sync
+                            //client.DownloadFileAsync(new Uri(downloadUrl), "test.mp3");//Async slecht
+                            await client.DownloadFileTaskAsync(downloadUrl, filename);//Async maar 1 per 1
+                                                                                      //await DownloadFileAsync(downloadUrl, client, filename);//Async
+                        }
+                    }
                 }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
 
-        //private async Task DownloadFileAsync(DocumentObject)
+        static private async Task DownloadFileAsync(string url, WebClient client, string naam)
+        {
+            try
+            {
+                using(WebClient wc = new WebClient())
+                {
+                    WebProxy wp = new WebProxy("35.161.2.200", 80);
+                    wc.Proxy = wp;
+                    //    await wc.DownloadFileTaskAsync(new Uri(url), "test.mp3");
+                    await client.DownloadFileTaskAsync(new Uri(url), naam);
+                }
+                //await client.DownloadFileTaskAsync(new Uri(url), naam);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
         //Het verloop van de download tonen
-        //static void Client_DownLoadProcessChanged(object sender, DownloadProgressChangedEventArgs e)
-        //{
-        //    int value = e.ProgressPercentage;
-        //}
+        static void Client_DownLoadProcessChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Console.WriteLine("Download " + e.ProgressPercentage + "%");
+        }
     }
 }
