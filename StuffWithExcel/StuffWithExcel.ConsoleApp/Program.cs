e5d43cfc33d0900be96c05d2ffa74486392ad493
Joren <jorenvandamme@hotmail.com>
2018-10-05 13:53:55 +0200

    [StuffWithExcel]File Startready with demo Excel
    - Added dummy Excel
    - Started with code to open an Excel.

--- StuffWithExcel/StuffWithExcel.ConsoleApp/Program.cs
+++ StuffWithExcel/StuffWithExcel.ConsoleApp/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
-namespace StuffWithExcel
+namespace StuffWithExcel.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"");
         }
     }
 }
--- StuffWithExcel/StuffWithExcel.ConsoleApp/Properties/AssemblyInfo.cs
+++ StuffWithExcel/StuffWithExcel.ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StuffWithExcel")]
+[assembly: AssemblyTitle("StuffWithExcel.ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StuffWithExcel")]
+[assembly: AssemblyProduct("StuffWithExcel.ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a941460-1fb0-4747-b247-0c431d5e0f15")]
+[assembly: Guid("1aa8e8cf-5c7c-4bf2-b72f-4f5cd9016fa4")]
 
 // Version information for an assembly consists of the following four values:
 //
